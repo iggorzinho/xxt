9c68187a7b1eadfbb885b67be5617335a93474b0
iggorzinho <iggor.rosario@gmail.com>
2017-01-02 23:43:22 -0300

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -104,9 +104,9 @@ function criaImagemBotao()
 	$(botao)["attr"]("id","imagemBotao");	
 	$(botao)["attr"]("src",imagemBotao["src"]);	
 	$(botao)["css"]("position","absolute");	
-	$(botao)["css"]("top","-26px");	
-	$(botao)["css"]("left","-10px");	
-	$(botao)["css"]("width","332px");	
+	$(botao)["css"]("top","-25px");	
+	$(botao)["css"]("left","0px");	
+	$(botao)["css"]("width","322px");	
 	$(botao)["css"]("height","58px");	
 	$(botao)["css"]("zIndex","2147483646");	
 	$(botao)["css"]("margin","0px");	
